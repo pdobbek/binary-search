eb0e5e3b79ea54eb123d0724adb2ffce23dd6677
pdobbek <p.dobbek@pm.me>
2022-06-21 20:38:57 +0100

    feat: enable binary search on the array

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const NUM_OF_BLOCKS = 30;
 var arrContainer = document.getElementById("array");
 /**
  * Function that draws an array of blocks of random heights.
@@ -7,13 +17,13 @@ function generateArray() {
     if (arrContainer != null) {
         let arr = [];
         // fill arr with random numbers
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < NUM_OF_BLOCKS; i++) {
             // gen a value from 1 to 100 (inclusive)
             const element = Number(Math.ceil(Math.random() * 100));
             arr.push(element);
         }
         arr.sort((a, b) => a - b); // sort arr
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < NUM_OF_BLOCKS; i++) {
             let value = arr[i];
             // make and style div
             let block = document.createElement("div");
@@ -22,12 +32,58 @@ function generateArray() {
             block.style.transform = `translate(${i * 30}px)`;
             // add label to each block
             var blockLabel = document.createElement("label");
-            blockLabel.classList.add("block_id");
+            blockLabel.classList.add("block_val");
             blockLabel.innerText = value.toString();
             block.appendChild(blockLabel);
             arrContainer.appendChild(block);
         }
     }
+}
+function binarySearch() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // block colours
+        const normalColour = "lightgreen";
+        const midColour = "orange";
+        const xColour = "red";
+        let blocks = document.querySelectorAll(".block");
+        let output = document.getElementById("output");
+        let x = Number(document.getElementById("x").value);
+        // reset in case of repeat use
+        output.innerText = "";
+        blocks.forEach(block => {
+            block.style.backgroundColor = normalColour;
+        });
+        let low = 0;
+        let high = NUM_OF_BLOCKS - 1;
+        let mid;
+        let isFound = false;
+        while (low <= high) {
+            mid = Math.floor((low + high) / 2);
+            blocks[mid].style.backgroundColor = midColour;
+            let midValue = Number(blocks[mid].getElementsByClassName("block_val")[0].innerHTML);
+            console.log("midValue = " + midValue);
+            // To wait for .15 sec
+            yield new Promise((resolve) => setTimeout(() => {
+                resolve();
+            }, 400));
+            if (midValue < x) {
+                low = mid + 1;
+                blocks[mid].style.backgroundColor = normalColour;
+            }
+            else if (midValue > x) {
+                high = mid - 1;
+                blocks[mid].style.backgroundColor = normalColour;
+            }
+            else {
+                blocks[mid].style.backgroundColor = xColour;
+                output.innerText = "Search done.";
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound)
+            output.innerText = "Number not in the array. Please try a different number.";
+    });
 }
 generateArray();
 console.log("test");
--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
+const NUM_OF_BLOCKS = 30;
+
 var arrContainer: HTMLElement | null = document.getElementById("array");
 
 /**
- * Function that draws an array of blocks of random heights.
+ * Draws an array of blocks of random heights.
  */
 function generateArray(): void {
     if (arrContainer != null) {
         let arr: number[] = [];
 
         // fill arr with random numbers
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < NUM_OF_BLOCKS; i++) {
             // gen a value from 1 to 100 (inclusive)
             const element: number = Number(Math.ceil(Math.random() * 100 ));
             arr.push(element);
@@ -16,7 +18,7 @@ function generateArray(): void {
         
         arr.sort((a, b) => a - b);  // sort arr
 
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < NUM_OF_BLOCKS; i++) {
             let value: number = arr[i];
 
             // make and style div
@@ -27,13 +29,66 @@ function generateArray(): void {
 
             // add label to each block
             var blockLabel: HTMLLabelElement = document.createElement("label");
-            blockLabel.classList.add("block_id");
+            blockLabel.classList.add("block_val");
             blockLabel.innerText = value.toString();
 
             block.appendChild(blockLabel);
             arrContainer.appendChild(block);
         }
     }   
+}
+
+/**
+ * Asynchronous binary search function. 
+ * Highlights the middle block as it searches.
+ */
+async function binarySearch(): Promise<void> {
+    // block colours
+    const normalColour: string = "lightgreen";
+    const midColour: string = "orange";
+    const xColour: string = "red";
+
+    let blocks: NodeListOf<HTMLDivElement> = document.querySelectorAll(".block");
+    let output: HTMLElement = document.getElementById("output")!;
+    let x: number = Number((document.getElementById("x") as HTMLInputElement).value);
+
+    // reset in case of repeat use
+    output.innerText = "";
+    blocks.forEach(block => {
+        block.style.backgroundColor = normalColour;
+    });
+
+    let low: number = 0;
+    let high: number = NUM_OF_BLOCKS - 1;
+    let mid: number;
+    let isFound: boolean = false;
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);
+        blocks[mid].style.backgroundColor = midColour;
+        let midValue: number = Number(blocks[mid].getElementsByClassName("block_val")[0].innerHTML);
+        console.log("midValue = " + midValue);
+
+        // To wait for .4 sec
+        await new Promise<void>((resolve) =>
+            setTimeout(() => {
+            resolve();
+            }, 400)
+        );
+
+        if (midValue < x) {
+            low = mid + 1;
+            blocks[mid].style.backgroundColor =  normalColour;
+        } else if (midValue > x) {
+            high = mid - 1;
+            blocks[mid].style.backgroundColor = normalColour;
+        } else {
+            blocks[mid].style.backgroundColor = xColour;
+            output.innerText = "Search done.";
+            isFound = true;
+            break;
+        }
+    }
+    if (!isFound) output.innerText = "Number not in the array. Please try a different number.";
 }
 
 generateArray();
